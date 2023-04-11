1048ca0e4c5d7a74aa5ff4d2013ce751492e080a
Arshad Anas <arshadanas900+github@gmail.com>
2023-04-11 16:40:15 -0400

    Converted low pass filter to FIR

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/LowPassFilter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/LowPassFilter.java
@@ -1,15 +1,28 @@
 package org.firstinspires.ftc.teamcode.control;
 
+import java.util.ArrayList;
+
 public class LowPassFilter {
     private double filterGain = 0.0;
+    private int pastEstimatesCount = 5;
     private double lastEstimate = 0.0;
+    private ArrayList<Double> pastEstimates = new ArrayList<Double>();
+
+    public void setGains (double estimateGain, int count) {
+        this.filterGain = estimateGain;
+        if (count == 0) {
+            count = 1;
+        }
+        pastEstimatesCount = count;
+    }
 
     public void setGains (double filterGain) {
         this.filterGain = filterGain;
     }
 
-    public void resetLastEstimate () {
-        lastEstimate = 0.0;
+    public void resetPastEstimates() {
+        pastEstimates.clear();
+        pastEstimates.add(0.0);
     }
 
     public double getLastEstimate () {
@@ -17,8 +30,19 @@ public class LowPassFilter {
     }
 
     public double getEstimate (double measurement) {
-        double output = filterGain * lastEstimate + (1 - filterGain) * measurement;
-        lastEstimate = measurement;
-        return output;
+        double total = 0.0;
+        for (Double estimate : pastEstimates) {
+            total += estimate;
+        }
+        lastEstimate = total / pastEstimatesCount;
+
+        double newEstimate = filterGain * lastEstimate + (1 - filterGain) * measurement;
+
+        pastEstimates.add(newEstimate);
+        if (pastEstimates.size() > pastEstimatesCount) {
+            pastEstimates.remove(0);
+        }
+
+        return newEstimate;
     }
 }
