58db9e564a9889e85241030f8527ccababe88d4d
Arshad Anas <arshadanas900+github@gmail.com>
2023-05-14 10:37:49 -0400

    COMBINED PIDF

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controller/PIDFController.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controller/PIDFController.kt
@@ -1,7 +1,6 @@
 package org.firstinspires.ftc.teamcode.control.controller
 
-import com.acmerobotics.roadrunner.util.NanoClock
-import org.firstinspires.ftc.teamcode.robot.RobotConfig
+import kotlin.math.abs
 import kotlin.math.max
 import kotlin.math.min
 import kotlin.math.sign
@@ -22,60 +21,23 @@ class PIDFController
  * @param kA feedforward acceleration gain
  * @param kStatic additive feedforward constant
  * @param maxIntegrationVelocity max velocity that integral path will continue integration
- * @param clock clock
  */
 @JvmOverloads constructor(
-    private var kP: Double,
-    private var kI: Double,
-    private var kD: Double,
-    private var filterGain: Double = 0.8,
-    private var kV: Double = 0.0,
-    private var kA: Double = 0.0,
-    private var kStatic: Double = 0.0,
-    private var maxIntegrationVelocity: Double = 1.0,
-    private val clock: NanoClock = NanoClock.system()
+    kP: Double,
+    kI: Double,
+    kD: Double,
+    kV: Double,
+    kA: Double = 0.0,
+    kStatic: Double = 0.0,
+    filterGain: Double = 0.8,
+    var maxIntegrationVelocity: Double = 1.0
 ) {
     private var outputBounded: Boolean = false
     private var minOutput: Double = 0.0
     private var maxOutput: Double = 0.0
 
-    private val PID: PIDController = PIDController(kP, kI, kD, filterGain)
-    private val FF: FeedForwardController = FeedForwardController(kV, kA, kStatic)
-
-    fun setGains(
-        kP: Double,
-        kI: Double,
-        kD: Double,
-        filterGain: Double,
-        kV: Double,
-        kA: Double,
-        kStatic: Double,
-        maxIntegrationVelocity: Double = this.maxIntegrationVelocity
-    ) {
-        PID.setGains(kP, kI, kD, filterGain)
-        FF.setGains(kV, kA, kStatic)
-        this.maxIntegrationVelocity = maxIntegrationVelocity
-    }
-
-    /**
-     * Target position (that is, the controller setpoint).
-     */
-    var targetPosition: Double = 0.0
-
-    /**
-     * Target velocity.
-     */
-    var targetVelocity: Double = 0.0
-
-    /**
-     * Target acceleration.
-     */
-    var targetAcceleration: Double = 0.0
-
-    /**
-     * Error computed in the last call to [update].
-     */
-    private var lastError: Double = 0.0
+    val PID: PIDController = PIDController(kP, kI, kD, filterGain)
+    val Feedforward: FeedforwardController = FeedforwardController(kV, kA, kStatic)
 
     /**
      * Sets bounds on the output of the controller.
@@ -84,53 +46,27 @@ class PIDFController
      * @param max maximum output
      */
     fun setOutputBounds(min: Double, max: Double) {
-        if (min < max) {
+        if (min != max) {
             outputBounded = true
-            minOutput = min
-            maxOutput = max
+            minOutput = min(min, max)
+            maxOutput = max(min, max)
             maxIntegrationVelocity = min(maxOutput, maxIntegrationVelocity)
         }
-    }
-
-    private fun getPositionError(measuredPosition: Double): Double {
-        return targetPosition - measuredPosition
-    }
-
-    fun atTargetPosition(measuredPosition: Double): Boolean {
-        return PID.atTargetPosition(measuredPosition)
-    }
-
-    fun setPositionTolerance(tolerance: Double) {
-        PID.positionTolerance = tolerance
     }
 
     /**
      * Run a single iteration of the controller.
      *
-     * @param measuredPosition measured position (feedback)
-     * @param measuredVelocity measured velocity
+     * @param measuredPosition measured position
      */
-    @JvmOverloads
-    fun update(
-        measuredPosition: Double,
-        measuredVelocity: Double? = null
-    ): Double {
+    fun update(measuredPosition: Double): Double {
         val PIDCommand: Double = PID.update(measuredPosition)
-        val FFCommand: Double = FF.update(PIDCommand)
+        val FFCommand: Double = Feedforward.update(PIDCommand)
         val output = PIDCommand + FFCommand
 
         PID.integrate =
-            Math.abs(output) < RobotConfig.LIFT_INTEGRATION_MAX_VELO || sign(output) != sign(PID.lastError)
+            abs(output) < maxIntegrationVelocity || sign(output) != sign(PID.lastError)
 
         return if (outputBounded) max(minOutput, min(output, maxOutput)) else output
-    }
-
-    /**
-     * Reset the controller's integral sum.
-     */
-    fun reset() {
-        PID.errorSum = 0.0
-        PID.lastError = 0.0
-        PID.integrate = true
     }
 }
