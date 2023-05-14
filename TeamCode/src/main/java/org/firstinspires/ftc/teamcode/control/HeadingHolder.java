d7667331741fe46dcd3405896b5763e43004c300
Arshad Anas <arshadanas900+github@gmail.com>
2023-05-14 07:41:53 -0400

    idk format

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/HeadingHolder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/HeadingHolder.java
@@ -3,16 +3,16 @@ package org.firstinspires.ftc.teamcode.control;
 //Class created to store the last known heading of the gyro
 public class HeadingHolder {
 
-    //lastHeading, accessed by get and setHeading. Starts at 0.
+    // lastHeading, accessed by get and setHeading. Starts at 0.
     private static double lastHeading = 0.0;
 
-    //Setter method
-    public static void setHeading (double newHeading){
+    // Setter method
+    public static void setHeading(double newHeading) {
         lastHeading = newHeading;
     }
 
-    //Getter method
-    public static double getHeading (){
+    // Getter method
+    public static double getHeading() {
         return lastHeading;
     }
 }
