2c5347bd51bebba8c1e04f596e56c5a979d4f8cf
Arshad Anas <arshadanas900+github@gmail.com>
2023-05-14 06:51:52 -0400

    separate PID and feedforward controllers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controller/PIDController.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controller/PIDController.kt
@@ -1,55 +1,33 @@
 package org.firstinspires.ftc.teamcode.control.controller
 
 import com.acmerobotics.roadrunner.util.NanoClock
-import com.acmerobotics.roadrunner.util.epsilonEquals
 import org.firstinspires.ftc.teamcode.control.filter.IIRLowPassFilter
 import kotlin.math.abs
-import kotlin.math.max
-import kotlin.math.min
 import kotlin.math.sign
 
 /**
- * PID controller with various feedforward components.
+ * PID controller
  */
 class PIDController
 /**
- * Constructor for [PIDController]. [kV], [kA], and [kStatic] are designed for DC motor feedforward
- * control (the most common kind of feedforward in FTC). [kF] provides a custom feedforward term for other plants.
- *
- * @param pid traditional PID coefficients
- * @param maxIntegrationVelocity max velocity that integral path will continue integration
- * @param filterGain derivative filter weight, 0 = unsmoothed, 0<x<1 increasingly smoothed, 1 = broken
- * @param kV feedforward velocity gain
- * @param kA feedforward acceleration gain
- * @param kStatic additive feedforward constant
- * @param kF custom feedforward that depends on position and/or velocity (e.g., a gravity term for arms)
+ * @param kP proportional gain
+ * @param kI integral gain
+ * @param kD derivative gain
+ * @param filterGain derivative filter weight, 0 = unsmoothed, 0 < x < 1 increasingly smoothed, 1 = broken
  * @param clock clock
  */
 @JvmOverloads constructor(
     private var kP: Double,
     private var kI: Double,
     private var kD: Double,
-    private var maxIntegrationVelocity: Double = 0.5,
     private var filterGain: Double = 0.8,
-    private var kV: Double = 0.0,
-    private var kA: Double = 0.0,
-    private var kStatic: Double = 0.0,
-    private var kF: (Double, Double?) -> Double = { _, _ -> 0.0 },
     private val clock: NanoClock = NanoClock.system()
 ) {
     private var errorSum: Double = 0.0
     private var lastUpdateTimestamp: Double = Double.NaN
 
-    private var inputBounded: Boolean = false
-    private var minInput: Double = 0.0
-    private var maxInput: Double = 0.0
-
-    private var outputBounded: Boolean = false
-    private var minOutput: Double = 0.0
-    private var maxOutput: Double = 0.0
-
     var currentFilterEstimate: Double = 0.0
-    private var integrate: Boolean = true
+    var integrate: Boolean = true
     var positionTolerance: Double = 2.0
 
     private var derivFilter: IIRLowPassFilter =
@@ -61,20 +39,12 @@ class PIDController
         kP: Double,
         kI: Double,
         kD: Double,
-        maxIntegrationVelocity: Double = this.maxIntegrationVelocity,
-        filterGain: Double = this.filterGain,
-        kV: Double = this.kV,
-        kA: Double = this.kA,
-        kStatic: Double = this.kStatic
+        filterGain: Double = this.filterGain
     ) {
         this.kP = kP
         this.kI = kI
         this.kD = kD
-        this.maxIntegrationVelocity = maxIntegrationVelocity
         this.filterGain = filterGain
-        this.kV = kV
-        this.kA = kA
-        this.kStatic = kStatic
         derivFilter.setGain(filterGain)
     }
 
@@ -84,59 +54,12 @@ class PIDController
     var targetPosition: Double = 0.0
 
     /**
-     * Target velocity.
-     */
-    var targetVelocity: Double = 0.0
-
-    /**
-     * Target acceleration.
-     */
-    var targetAcceleration: Double = 0.0
-
-    /**
      * Error computed in the last call to [update].
      */
     private var lastError: Double = 0.0
 
-//    /**
-//     * Sets bound on the input of the controller. The min and max values are considered modularly-equivalent (that is,
-//     * the input wraps around).
-//     *
-//     * @param min minimum input
-//     * @param max maximum input
-//     */
-//    fun setInputBounds(min: Double, max: Double) {
-//        if (min < max) {
-//            inputBounded = true
-//            minInput = min
-//            maxInput = max
-//        }
-//    }
-
-    /**
-     * Sets bounds on the output of the controller.
-     *
-     * @param min minimum output
-     * @param max maximum output
-     */
-    fun setOutputBounds(min: Double, max: Double) {
-        if (min < max) {
-            outputBounded = true
-            minOutput = min
-            maxOutput = max
-            maxIntegrationVelocity = min(maxOutput, maxIntegrationVelocity)
-        }
-    }
-
     private fun getPositionError(measuredPosition: Double): Double {
-        var error = targetPosition - measuredPosition
-        if (inputBounded) {
-            val inputRange = maxInput - minInput
-            while (abs(error) > inputRange / 2.0) {
-                error -= sign(error) * inputRange
-            }
-        }
-        return error
+        return targetPosition - measuredPosition
     }
 
     fun atTargetPosition(measuredPosition: Double): Boolean {
@@ -146,14 +69,9 @@ class PIDController
     /**
      * Run a single iteration of the controller.
      *
-     * @param measuredPosition measured position (feedback)
-     * @param measuredVelocity measured velocity
+     * @param measuredPosition measured position
      */
-    @JvmOverloads
-    fun update(
-        measuredPosition: Double,
-        measuredVelocity: Double? = null
-    ): Double {
+    fun update(measuredPosition: Double): Double {
         val currentTimestamp = clock.seconds()
         val error = getPositionError(measuredPosition)
         val dy = error - lastError
@@ -173,23 +91,7 @@ class PIDController
             lastError = error
             lastUpdateTimestamp = currentTimestamp
 
-            // note: we'd like to refactor this with Kinematics.calculateMotorFeedforward() but kF complicates the
-            // determination of the sign of kStatic
-            val baseOutput = kP * error +
-                    kI * errorSum +
-                    kD * (measuredVelocity?.let { targetVelocity - it } ?: errorDeriv) +
-                    kV * targetVelocity +
-                    kA * targetAcceleration +
-                    kF(
-                        measuredPosition,
-                        measuredVelocity
-                    )
-            val output =
-                if (baseOutput epsilonEquals 0.0) 0.0 else baseOutput + sign(baseOutput) * kStatic
-
-            integrate = !(abs(output) >= maxIntegrationVelocity && sign(output) == sign(error))
-
-            if (outputBounded) max(minOutput, min(output, maxOutput)) else output
+            (kP * error + kI * errorSum + kD * errorDeriv)
         }
     }
 
