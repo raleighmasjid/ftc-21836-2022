b4d2c9b8bb2cec52754d74a266affe8da6785ebd
Yunus Gul <nunus.trains@gmail.com>
2023-06-18 15:27:06 -0400

    Progress
    moremoremore

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/MainTeleOp.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode.opmodes;
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -19,6 +21,11 @@ public class MainTeleOp extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
 
         // Do stuff on init
+        MotorEx leftFront = new MotorEx(hardwareMap, "leftfront");
+        MotorEx leftBack = new MotorEx(hardwareMap, "leftback");
+        MotorEx rightFront = new MotorEx(hardwareMap, "rightfront");
+        MotorEx rightBack = new MotorEx(hardwareMap, "rightback");
+        MotorEx liftMotor = new MotorEx(hardwareMap, "liftmotor");
 
         // Initialize telemetry
         myTelemetry = new MultipleTelemetry(telemetry, FtcDashboard.getInstance().getTelemetry());
@@ -27,13 +34,29 @@ public class MainTeleOp extends LinearOpMode {
         Gamepad1 = new GamepadEx(gamepad1);
         Gamepad2 = new GamepadEx(gamepad2);
 
-
+        double moveYLeft;
+        double moveYRight;
         waitForStart();
 
         //teleop control loop
         while (opModeIsActive()) {
 
             // Do stuff while op mode is active
+
+            moveYLeft = Gamepad1.getLeftY();
+            leftFront.set(moveYLeft);
+            leftBack.set(moveYLeft);
+
+
+
+
+            moveYRight = Gamepad1.getRightY();
+            rightFront.set(moveYRight);
+            rightBack.set(moveYRight);
+
+
+
+
 
         }
     }
