93d34809358c5940eb97a6ecbc8dcee7e80ad827
Arshad Anas <arshadanas900+github@gmail.com>
2023-07-05 17:13:00 -0400

    Create ProfiledPIDF.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controller/ProfiledPIDF.java
@@ -0,0 +1,96 @@
+package org.firstinspires.ftc.teamcode.control.controller;
+
+import com.acmerobotics.roadrunner.profile.MotionProfile;
+import com.acmerobotics.roadrunner.profile.MotionProfileGenerator;
+import com.acmerobotics.roadrunner.profile.MotionState;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class ProfiledPIDF extends PIDFController {
+
+    protected MotionProfile profile;
+
+    private MotionState profileState = new MotionState(0.0, 0.0, 0.0, 0.0);
+
+    private final ElapsedTime profileTimer = new ElapsedTime();
+
+    private double MAX_VELO = 1.0;
+    private double MAX_ACCEL = 1.0;
+    private double MAX_JERK = 0.0;
+
+    /**
+     * Initialize fields <p>
+     * Use {@link #updateConstraints} to update constants
+     *
+     * @param pid         PID feedback controller
+     * @param feedforward kV-kA-kS feedforward controller
+     */
+    public ProfiledPIDF(PIDController pid, FeedforwardController feedforward) {
+        super(pid, feedforward);
+        profileTimer.reset();
+    }
+
+    public ProfiledPIDF() {
+        this(new PIDController(), new FeedforwardController());
+    }
+
+    public void setTargetPosition(double currentPosition, double currentVelocity, double targetPosition) {
+        profile = MotionProfileGenerator.generateSimpleMotionProfile(
+                new MotionState(currentPosition, currentVelocity),
+                new MotionState(targetPosition, 0.0),
+                MAX_VELO,
+                MAX_ACCEL,
+                MAX_JERK
+        );
+        profileTimer.reset();
+    }
+
+    public void setTargetPosition(double currentPosition, double targetPosition) {
+        setTargetPosition(currentPosition, 0.0, targetPosition);
+    }
+
+    public void updateConstraints(double MAX_VELO, double MAX_ACCEL, double MAX_JERK) {
+        this.MAX_VELO = MAX_VELO;
+        this.MAX_ACCEL = MAX_ACCEL;
+        this.MAX_JERK = MAX_JERK;
+    }
+
+    public void updateConstraints(double MAX_VELO, double MAX_ACCEL) {
+        updateConstraints(MAX_VELO, MAX_ACCEL, MAX_JERK);
+    }
+
+    @Override
+    public double update(double measuredPosition, double voltage) {
+        profileState = profile.get(profileTimer.seconds());
+
+        pid.setTarget(profileState.getX());
+        feedforward.setTargetVelocity(profileState.getV());
+        feedforward.setTargetAcceleration(profileState.getA());
+
+        return super.update(measuredPosition, voltage);
+    }
+
+    @Override
+    public double update(double measuredPosition) {
+        return this.update(measuredPosition, 12.0);
+    }
+
+    public double getProfilePosition() {
+        return profileState.getX();
+    }
+
+    public double getProfileVelocity() {
+        return profileState.getV();
+    }
+
+    public double getProfileAcceleration() {
+        return profileState.getA();
+    }
+
+    public double getProfileJerk() {
+        return profileState.getJ();
+    }
+
+    public boolean isDone() {
+        return profileTimer.seconds() > profile.duration();
+    }
+}
