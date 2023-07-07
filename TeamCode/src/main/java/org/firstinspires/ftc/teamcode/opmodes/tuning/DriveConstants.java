4579d26b442c54cfc370ba7bb7fc076076989634
Arshad Anas <arshadanas900+github@gmail.com>
2023-07-06 20:30:17 -0400

    Revert velo ff tuning for velo PID instead
    This reverts commit 04941748863a812277b7f8fe4328f2286a9bddca.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/tuning/DriveConstants.java
@@ -31,7 +31,7 @@ public class DriveConstants {
      * If using the built-in motor velocity PID, update MOTOR_VELO_PID with the tuned coefficients
      * from DriveVelocityPIDTuner.
      */
-    public static final boolean RUN_USING_ENCODER = false;
+    public static final boolean RUN_USING_ENCODER = true;
     public static PIDFCoefficients MOTOR_VELO_PID = new PIDFCoefficients(15, 1.5, 5, 11);
 
     /*
@@ -52,8 +52,8 @@ public class DriveConstants {
      * motor encoders or have elected not to use them for velocity control, these values should be
      * empirically tuned.
      */
-    public static double kV = 0.02;
-    public static double kA = 0.00375;
+    public static double kV = 1.0 / rpmToVelocity(MAX_RPM);
+    public static double kA = 0;
     public static double kStatic = 0;
 
     /*
