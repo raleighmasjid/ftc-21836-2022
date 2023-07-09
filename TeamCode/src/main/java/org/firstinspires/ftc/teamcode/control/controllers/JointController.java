b445b3c2e48b2dda096402fddea96a88c4bad02b
Arshad Anas <arshadanas900+github@gmail.com>
2023-07-09 16:46:49 -0400

    block access to setTarget(MotionState) from instances of ProfiledJointController

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controllers/JointController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controllers/JointController.java
@@ -2,9 +2,6 @@ package org.firstinspires.ftc.teamcode.control.controllers;
 
 import com.acmerobotics.roadrunner.profile.MotionState;
 
-/**
- * Wrapper class combining a PID controller and a kV-kA-kS feedforward controller.
- */
 public class JointController implements FeedbackController {
     public final FeedbackController feedback;
     public final FeedforwardController feedforward;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controllers/ProfiledJointController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controllers/ProfiledJointController.java
@@ -4,19 +4,30 @@ import com.acmerobotics.roadrunner.profile.MotionState;
 
 import org.firstinspires.ftc.teamcode.control.MotionProfiler;
 
-public class ProfiledController extends JointController implements FeedbackController {
+public class ProfiledJointController extends JointController implements FeedbackController {
 
     public MotionProfiler profiler = new MotionProfiler();
 
-    public ProfiledController(FeedbackController feedback, FeedforwardController feedforward) {
+    public ProfiledJointController(FeedbackController feedback, FeedforwardController feedforward) {
         super(feedback, feedforward);
     }
 
+    @Override
+    public void setTarget(MotionState targetState) {
+        profiler.setTarget(profiler.update(), targetState);
+    }
+
+    public void setTarget(MotionState measuredState, MotionState targetState) {
+        profiler.setTarget(measuredState, targetState);
+    }
+
+    @Override
     public double calculate(MotionState measuredState, double voltage) {
         super.setTarget(profiler.update());
         return super.calculate(measuredState, voltage);
     }
 
+    @Override
     public double calculate(MotionState measuredState) {
         return this.calculate(measuredState, 12.0);
     }
