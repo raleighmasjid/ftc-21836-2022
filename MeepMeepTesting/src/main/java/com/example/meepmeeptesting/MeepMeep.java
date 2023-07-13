8e2a2d9381f2ee4c745eba201632b16a52d37f11
Arshad Anas <arshadanas900+github@gmail.com>
2023-07-13 15:49:48 -0400

    update meepmeep

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeep.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeep.java
@@ -7,6 +7,7 @@ import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
 
 public class MeepMeep {
+
     public static double
             MED_ANGLE = 35,
             MED_X = 31,
@@ -33,7 +34,13 @@ public class MeepMeep {
             TIME_DROP_TO_FLIP = 1,
             TIME_FIRST_FLIP = 1,
             TIME_LIFT_MEDIUM = 0.8,
-            TIME_LIFT_TALL = 1.0;
+            TIME_LIFT_TALL = 1.0,
+            TOTAL_RUN_TIME = 30.0;
+
+    public static final double
+            RIGHT = Math.toRadians(0),
+            FORWARD = Math.toRadians(90),
+            LEFT = Math.toRadians(180);
 
     public static void main(String[] args) {
         com.noahbres.meepmeep.MeepMeep meepMeep = new com.noahbres.meepmeep.MeepMeep(700);
@@ -43,10 +50,6 @@ public class MeepMeep {
 
         double X_START = side * ZONE_2_X;
 
-        double facingRight = Math.toRadians(0);
-        double facingForward = Math.toRadians(90);
-        double facingLeft = Math.toRadians(180);
-
         Vector2d stackPos = new Vector2d(side * STACK_X, STACK_Y);
         Vector2d sideTurnPos = new Vector2d(side * X_TURN_POS, Y_MAIN_PATH);
         Pose2d tallScoringPos = new Pose2d(side * TALL_X, TALL_Y, Math.toRadians(isRight ? TALL_ANGLE : 180 - TALL_ANGLE));
@@ -54,19 +57,18 @@ public class MeepMeep {
         Vector2d centerTurnPos = new Vector2d(sideTurnPos.getX() - side * ONE_TILE, sideTurnPos.getY());
         Pose2d centerTallScoringPos = new Pose2d(medScoringPos.getX() - side * ONE_TILE, medScoringPos.getY(), medScoringPos.getHeading());
 
-        Pose2d parkingZone1 = new Pose2d(side * (isRight ? ZONE_1_X : ZONE_3_X), Y_MAIN_PATH, isRight ? facingRight : facingLeft);
-        Pose2d parkingZone2 = new Pose2d(X_START, Y_MAIN_PATH, parkingZone1.getHeading());
-        Pose2d parkingZone3 = new Pose2d(side * (isRight ? ZONE_3_X : ZONE_1_X), Y_MAIN_PATH, parkingZone1.getHeading());
+        double zone1x = side * (isRight ? ZONE_1_X : ZONE_3_X);
+        double zone3x = side * (isRight ? ZONE_3_X : ZONE_1_X);
+        Pose2d parkingZone1 = new Pose2d(zone1x, Y_MAIN_PATH, isRight ? RIGHT : LEFT);
+        Pose2d parkingZone2 = new Pose2d(X_START, parkingZone1.getY(), parkingZone1.getHeading());
+        Pose2d parkingZone3 = new Pose2d(zone3x, parkingZone1.getY(), parkingZone1.getHeading());
 
-        Pose2d startPose = new Pose2d(X_START, Y_START, facingForward);
+        Pose2d startPose = new Pose2d(X_START, Y_START, FORWARD);
 
-        double TIME_LIFT;
-        Pose2d scoringPos;
-        double stackOffset;
-
-        TIME_LIFT = TIME_LIFT_TALL;
-        scoringPos = tallScoringPos;
-        stackOffset = side * Math.toRadians(STACK_ANGLE_OFFSET_TALL);
+        boolean tall = false;
+        double TIME_LIFT = tall ? TIME_LIFT_TALL : TIME_LIFT_MEDIUM;
+        Pose2d scoringPos = tall ? tallScoringPos : medScoringPos;
+        double sideTurnPosAngleOffset = side * Math.toRadians(tall ? STACK_ANGLE_OFFSET_TALL : STACK_ANGLE_OFFSET_MED);
 
         RoadRunnerBotEntity bot1 = new DefaultBotBuilder(meepMeep)
                 .setDimensions(17, 16)
@@ -80,41 +82,31 @@ public class MeepMeep {
 //                                .UNSTABLE_addTemporalMarkerOffset(-TIME_FIRST_FLIP, () -> scorer.passthrough.trigger())
 //                                .UNSTABLE_addTemporalMarkerOffset(-TIME_LIFT, () -> scorer.setTargetLiftPos(pole))
                                         .waitSeconds(TIME_PRE_DROP)
-//                                .addTemporalMarker(() -> scorer.dropCone(PowerplayLift.Position.FIVE))
+//                                .addTemporalMarker(() -> scorer.dropCone(Lift.Position.FIVE))
                                         .waitSeconds(TIME_DROP)
 //                                .UNSTABLE_addTemporalMarkerOffset(TIME_DROP_TO_FLIP, () -> scorer.passthrough.trigger())
-                                        .setReversed(false)
-                                        .splineTo(sideTurnPos, stackOffset + (isRight ? facingRight : facingLeft))
-                                        .splineTo(stackPos, isRight ? facingRight : facingLeft)
+                                        .splineTo(sideTurnPos, sideTurnPosAngleOffset + (isRight ? RIGHT : LEFT))
+                                        .splineTo(stackPos, isRight ? RIGHT : LEFT)
                                         // loop below
+                                        .waitSeconds(TIME_PRE_GRAB)
+//                                .addTemporalMarker(scorer::grabCone)
+                                        .waitSeconds(TIME_GRAB)
+//                                .UNSTABLE_addTemporalMarkerOffset(TIME_GRAB_TO_FLIP, scorer.passthrough::trigger)
+                                        .setReversed(true)
+                                        .splineTo(sideTurnPos, sideTurnPosAngleOffset + (isRight ? LEFT : RIGHT))
+                                        .splineToSplineHeading(scoringPos, scoringPos.getHeading() - LEFT)
+//                                .UNSTABLE_addTemporalMarkerOffset(-TIME_LIFT, () -> scorer.setTargetLiftPos(pole))
+                                        .waitSeconds(TIME_PRE_DROP)
+//                                .addTemporalMarker(() -> scorer.dropCone(endLiftPosition))
+                                        .waitSeconds(TIME_DROP)
+//                                .UNSTABLE_addTemporalMarkerOffset(TIME_DROP_TO_FLIP, scorer.passthrough::trigger)
+                                        .setReversed(false)
+                                        .splineTo(sideTurnPos, sideTurnPosAngleOffset + (isRight ? RIGHT : LEFT))
+                                        .splineTo(stackPos, isRight ? RIGHT : LEFT)
+                                        // common parking:
                                         .waitSeconds(TIME_PRE_GRAB)
 //                                .addTemporalMarker(() -> scorer.grabCone())
                                         .waitSeconds(TIME_GRAB)
-//                                .UNSTABLE_addTemporalMarkerOffset(TIME_GRAB_TO_FLIP, () -> scorer.passthrough.trigger())
-                                        .setReversed(true)
-                                        .splineTo(sideTurnPos, stackOffset + (isRight ? facingLeft : facingRight))
-                                        .splineToSplineHeading(scoringPos, scoringPos.getHeading() - facingLeft)
-//                                .UNSTABLE_addTemporalMarkerOffset(-TIME_LIFT, () -> scorer.setTargetLiftPos(pole))
-                                        .waitSeconds(TIME_PRE_DROP)
-//                                .addTemporalMarker(() -> scorer.dropCone(PowerplayLift.Position.FOUR))
-                                        .waitSeconds(TIME_DROP)
-                                        .setReversed(false)
-                                        .splineTo(sideTurnPos, stackOffset + (isRight ? facingRight : facingLeft))
-                                        .splineTo(stackPos, isRight ? facingRight : facingLeft)
-                                        .setReversed(true)
-                                        .splineTo(centerTurnPos, facingLeft)
-                                        .splineToSplineHeading(centerTallScoringPos, medScoringPos.getHeading() - facingLeft)
-//                                        .UNSTABLE_addTemporalMarkerOffset(-TIME_LIFT_TALL, () -> scorer.setTargetLiftPos(PowerplayLift.Position.TALL))
-                                        .waitSeconds(TIME_PRE_DROP)
-//                                        .addTemporalMarker(() -> scorer.dropCone())
-                                        .waitSeconds(TIME_DROP)
-//                                        .UNSTABLE_addTemporalMarkerOffset(TIME_DROP_TO_FLIP, () -> scorer.passthrough.trigger())
-                                        .lineToSplineHeading(parkingZone1)
-                                        // common parking
-                                        .waitSeconds(TIME_PRE_GRAB)
-//                                        .addTemporalMarker(() -> scorer.grabCone())
-                                        .waitSeconds(TIME_GRAB)
-//                                        .UNSTABLE_addTemporalMarkerOffset(TIME_GRAB_TO_FLIP, () -> scorer.passthrough.trigger())
                                         .build()
                 );
 
