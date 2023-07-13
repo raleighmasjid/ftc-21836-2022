01db930f75b32fef47da819b1e7af48d1914102f
Arshad Anas <arshadanas900+github@gmail.com>
2023-07-13 19:33:06 -0400

    Create FullStateController.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controllers/FullStateController.java
@@ -0,0 +1,28 @@
+package org.firstinspires.ftc.teamcode.control.controllers;
+
+import org.firstinspires.ftc.teamcode.control.State;
+import org.firstinspires.ftc.teamcode.control.controllers.gainmatrices.FullStateGains;
+
+public class FullStateController implements FeedbackController {
+
+    private FullStateGains gains;
+    private State target;
+
+    @Override
+    public void setTarget(State target) {
+        this.target = target;
+    }
+
+    public void setGains(FullStateGains gains) {
+        this.gains = gains;
+    }
+
+    @Override
+    public double calculate(State measurement) {
+        double pError = target.getX() - measurement.getX();
+        double vError = target.getV() - measurement.getV();
+        double aError = target.getA() - measurement.getA();
+
+        return (pError * gains.getPGain()) + (vError * gains.getVGain()) + (aError * gains.getAGain());
+    }
+}
