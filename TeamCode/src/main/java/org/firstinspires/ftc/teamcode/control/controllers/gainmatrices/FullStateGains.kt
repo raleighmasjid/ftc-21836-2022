4002810cc3eea8a35f017b92c5a08fc72ed67847
Arshad Anas <arshadanas900+github@gmail.com>
2023-07-13 19:25:15 -0400

    default values in PIDGains, FullStateGains data class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controllers/gainmatrices/FullStateGains.kt
@@ -0,0 +1,7 @@
+package org.firstinspires.ftc.teamcode.control.controllers.gainmatrices
+
+data class FullStateGains @JvmOverloads constructor(
+    val pGain: Double = 0.0,
+    val vGain: Double = 0.0,
+    val aGain: Double = 0.0
+)
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controllers/gainmatrices/PIDGains.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/controllers/gainmatrices/PIDGains.kt
@@ -1,8 +1,8 @@
 package org.firstinspires.ftc.teamcode.control.controllers.gainmatrices
 
 data class PIDGains @JvmOverloads constructor(
-    val kP: Double,
-    val kI: Double,
-    val kD: Double,
+    val kP: Double = 0.0,
+    val kI: Double = 0.0,
+    val kD: Double = 0.0,
     val maxOutputWithIntegral: Double = Double.POSITIVE_INFINITY
 )
