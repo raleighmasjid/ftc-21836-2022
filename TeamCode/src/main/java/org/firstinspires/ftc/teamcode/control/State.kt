3891b306c36f20ff1ef8de142508e38655ef279c
Arshad Anas <arshadanas900+github@gmail.com>
2023-07-14 13:14:30 -0400

    default (0, 0, 0) values for States

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/State.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/State.kt
@@ -1,7 +1,7 @@
 package org.firstinspires.ftc.teamcode.control
 
 data class State @JvmOverloads constructor(
-    val x: Double,
+    val x: Double = 0.0,
     val v: Double = 0.0,
     val a: Double = 0.0
 )
