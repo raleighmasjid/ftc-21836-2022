a2bc217ec65e96258706b57247048943d8aa40d0
Arshad Anas <arshadanas900+github@gmail.com>
2023-07-15 06:23:06 -0400

    move out of package for training purposes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagCamera.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AprilTagCamera.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.apriltags;
+package org.firstinspires.ftc.teamcode;
 
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
