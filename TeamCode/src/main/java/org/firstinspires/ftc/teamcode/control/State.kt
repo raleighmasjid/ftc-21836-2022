844aea2d1d9f2f3b366c79bd704dbbb80dfe74bc
Arshad Anas <arshadanas900+github@gmail.com>
2023-07-16 06:02:50 -0400

    add j field to State.kt

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/State.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/control/State.kt
@@ -4,4 +4,5 @@ data class State @JvmOverloads constructor(
     @JvmField val x: Double = 0.0,
     @JvmField val v: Double = 0.0,
     @JvmField val a: Double = 0.0,
+    @JvmField val j: Double = 0.0,
 )
